aa78715bf70525f30e67f020336f0f1457cd7fe9
elias <eliassilva0045@gmail.com>
2024-05-16 15:10:31 -0300

    Create POO challenge project

---
+++ README.md
@@ -0,0 +1,5 @@
+# Challenge POO
+
+Modelagem e diagramação da representação em UML e Código no que se refere a um iPhone.
+
+![Diagrama de Classe](./challenge.poo/docs/iPhone%20UML.png)
---
+++ challenge.poo/src/Usuario.java
@@ -0,0 +1,8 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        Iphone iPhone = new Iphone();
+
+        iPhone.ligar();
+        iPhone.tocar();
+    }
+}
